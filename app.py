24da12c5f1b857b29a8824ec04a0c3428b84aa09
Andile Mgazi <vuyiswaandile176@gmail.com>
2025-10-03 19:54:27 +0200

    fixing my port

--- app.py
+++ app.py
@@ -164,5 +164,5 @@ def unread_count():
     return {'unread': count}
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
